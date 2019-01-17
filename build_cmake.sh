31808cc6b40731e9ff227ef3ab72607461c22189
TQA <trinhquocanh94@gmail.com>
2019-01-18 00:11:41 +0100

    Modify script to automatically do CMake for new exercises.

--- build_cmake.sh
+++ build_cmake.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 # Automatically build all neccessary files for an exercise
-cd $1
-echo $1
-file_name=${1/-/_}
-echo $file_name
-touch $file_name.h $file_name.cpp
-echo $file_name
-mkdir build
-cd build
-cmake -G "Unix Makefiles" ..
+for path in */; do
+    [ -d "${path}" ] || continue # if not a directory, skip
+    dirname="$(basename "${path}")"
+    cd $dirname
+    echo $path
+    if ! [ -d "./build" ]; then
+      file_name=${path::-1}
+      file_name=${file_name/-/_}
+      echo $file_name
+      touch $file_name.h $file_name.cpp
+      echo $file_name
+      mkdir build
+      cd build
+      cmake -G "Unix Makefiles" ..
+      cd ../..
+    else
+      cd ..
+    fi
+done
