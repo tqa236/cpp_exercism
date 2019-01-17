f27e57ddee705c2fbd0b927e22520b668f7715b1
TQA <trinhquocanh94@gmail.com>
2019-01-17 23:47:04 +0100

    Modify build_cmake script to generate the correct name and add shebang for portability.

--- build_cmake.sh
+++ build_cmake.sh
@@ -1,6 +1,11 @@
+#!/usr/bin/env bash
 # Automatically build all neccessary files for an exercise
 cd $1
-touch $1.h $1.cpp
+echo $1
+file_name=${1/-/_}
+echo $file_name
+touch $file_name.h $file_name.cpp
+echo $file_name
 mkdir build
 cd build
 cmake -G "Unix Makefiles" ..
---
+++ hexadecimal/hexadecimal.h
@@ -0,0 +1,8 @@
+#include <string>
+using namespace std;
+
+namespace hexadecimal {
+
+int convert(string hex);
+
+}
