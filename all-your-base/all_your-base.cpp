498b7ea4aa35d8c193504ff3351fe3cc90d4b0cd
TQA <trinhquocanh94@gmail.com>
2019-01-19 19:04:11 +0100

    Add new exercises.

---
+++ all-your-base/all_your-base.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+namespace all_your_base {
+  int convert(unsigned int in_base, string in_digits, unsigned int out_base){
+    return 1;
+    // int dec = 0;
+    // int l = in_digits.length();
+    // for (int i = 0; i < l; i++){
+    //   int num = (int) in_digits[i];
+    //   if ((num >= 48) && (num <= 49)){
+    //     num = num - (int)'0';
+    //   }
+    //   else return 0;
+    //   dec = 2*dec + num;
+    // }
+    // return dec;
+  }
+}
---
+++ all-your-base/all_your-base.h
@@ -0,0 +1,8 @@
+#include <string>
+using namespace std;
+
+namespace all_your_base {
+
+int convert(unsigned int in_base, string in_digits, unsigned int out_base);
+
+}
