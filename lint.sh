da5113080d450d99c5d499dc76849d6bc654656e
TQA <trinhquocanh94@gmail.com>
2019-02-16 22:39:18 +0100

    Try to install cpplint on travis

--- lint.sh
+++ lint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-apt-get install libffi-dev libssl-dev
+sudo apt-get install libffi-dev libssl-dev
 pip install requests[security]
 pip install cpplint
 find . -iname "*.cpp" | grep -v "test" | grep -v "Compiler" |  xargs cpplint --filter=-legal/copyright,-build/include_subdir
