bfe55ddc9c42e409e8350a5ee90107c66fffe867
TQA <trinhquocanh94@gmail.com>
2019-02-16 20:53:19 +0100

    Try exercism travis config

--- clock/clock.h
+++ clock/clock.h
@@ -1,15 +1,15 @@
 #pragma once
 #include <string>
 
-namespace date_independent {
-    using namespace std;
-
-    class clock{
-    private:
-        int hour;
-        int minute;
-    public:
-        static clock at(int, int);
-        operator std::string() const;
-    }
-}
+// namespace date_independent {
+//     using namespace std;
+//
+//     class clock{
+//     private:
+//         int hour;
+//         int minute;
+//     public:
+//         static clock at(int, int);
+//         operator std::string() const;
+//     }
+// }
