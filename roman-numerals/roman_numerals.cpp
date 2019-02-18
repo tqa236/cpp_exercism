8a8215e572af60794bcdcec03fd53f1fa5681d8b
TQA <trinhquocanh94@gmail.com>
2019-02-18 22:53:04 +0100

    Refactor

--- roman-numerals/roman_numerals.cpp
+++ roman-numerals/roman_numerals.cpp
@@ -11,12 +11,10 @@ string roman::convert(int number) {
                             {100, "C"}, {500, "D"}, {1000, "M"}};
   vector<int> addition = {1000, 500, 100, 50, 10, 5, 1};
   vector<int> substraction = {100, 100, 10, 10, 1, 1};
-  int addition_number;
-  int substraction_number;
   string roman_number = "";
   while (number > 0) {
-    addition_number = addition.front();
-    substraction_number = substraction.front();
+    int addition_number = addition.front();
+    int substraction_number = substraction.front();
     if (number < addition_number - substraction_number) {
       addition.erase(addition.begin());
       substraction.erase(substraction.begin());
