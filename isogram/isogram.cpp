9a992a0aadcead5ed2ff499e01385592764a3634
TQA <trinhquocanh94@gmail.com>
2019-03-12 15:10:31 +0100

    Solve isogram

---
+++ isogram/isogram.cpp
@@ -0,0 +1,21 @@
+#include "isogram.h"
+#include <algorithm>
+#include <bitset>
+#include <stdexcept>
+
+using std::bitset;
+using std::tolower;
+
+bool isogram::is_isogram(string phrase) {
+  bitset<26> letters;
+  for (char c : phrase) {
+    int position = tolower(c) - 'a';
+    if ((position >= 0) && (position <= 25)) {
+      if (letters[position] == 1) {
+        return false;
+      }
+      letters.set(position);
+    }
+  }
+  return true;
+}
---
+++ isogram/isogram.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <string>
+
+namespace isogram {
+using std::string;
+bool is_isogram(string);
+}
