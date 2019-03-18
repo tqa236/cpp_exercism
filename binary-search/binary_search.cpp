cc34cc3f491920766f3be5079d45f4ec4d67a08b
TQA <trinhquocanh94@gmail.com>
2019-03-18 20:13:22 +0100

    Solve clock and binary search

---
+++ binary-search/binary_search.cpp
@@ -0,0 +1,18 @@
+#include "binary_search.h"
+#include <algorithm>
+#include <stdexcept>
+
+using std::domain_error;
+
+namespace binary_search {
+
+size_t find(vector<int> data, int number) {
+  auto it = std::find(data.begin(), data.end(), number);
+  if (it != data.end()) {
+    return it - data.begin();
+  } else {
+    throw domain_error("This number is not existed in the vector.");
+  }
+}
+
+}  // namespace binary_search
---
+++ binary-search/binary_search.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <vector>
+
+namespace binary_search {
+using std::vector;
+using std::size_t;
+
+size_t find(vector<int>, int);
+}
