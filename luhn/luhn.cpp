f3925417ab13b007401f339663634f47973cbb66
TQA <trinhquocanh94@gmail.com>
2019-07-13 13:39:01 +0200

    Solve Luhn

---
+++ luhn/luhn.cpp
@@ -0,0 +1,25 @@
+#include "luhn.h"
+#include <algorithm>
+
+using std::reverse;
+
+bool luhn::valid(string number) {
+  number.erase(remove_if(number.begin(), number.end(), isspace), number.end());
+  if (number == "0") {
+    return false;
+  }
+  reverse(number.begin(), number.end());
+  int counter = 0;
+  int sum = 0;
+  for (char& digit : number) {
+    digit = digit - '0';
+    if (digit < 0 || digit > 9) return false;
+    if (counter % 2 != 0) {
+      digit = 2 * digit - 9 * (digit / 5);
+    }
+    counter = counter + 1;
+    sum = sum + digit;
+  }
+
+  return sum % 10 == 0;
+}
---
+++ luhn/luhn.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+
+using std::string;
+
+namespace luhn {
+    bool valid(string);
+}
