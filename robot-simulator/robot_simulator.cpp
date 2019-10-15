d0ac977fe16f684075936538ea90ac0019787e19
TQA <trinhquocanh94@gmail.com>
2019-10-15 20:34:50 +0200

    Improve robot simulator

--- robot-simulator/robot_simulator.cpp
+++ robot-simulator/robot_simulator.cpp
@@ -2,35 +2,39 @@
 
 namespace robot_simulator {
 
+Robot::Robot() = default;
+Robot::Robot(Coordination robot_position, Bearing robot_bearing)
+    : robot_position{robot_position}, robot_bearing{robot_bearing} {}
+
 void Robot::turn_right() {
-  robot_bearing = robot_bearing == Bearing::WEST
-                      ? Bearing::NORTH
-                      : static_cast<Bearing>(robot_bearing + 1);
+  robot_bearing =
+      robot_bearing == WEST ? NORTH : static_cast<Bearing>(robot_bearing + 1);
 }
+
 void Robot::turn_left() {
-  robot_bearing = robot_bearing == Bearing::NORTH
-                      ? Bearing::WEST
-                      : static_cast<Bearing>(robot_bearing - 1);
+  robot_bearing =
+      robot_bearing == NORTH ? WEST : static_cast<Bearing>(robot_bearing - 1);
 }
+
 void Robot::advance() {
   switch (robot_bearing) {
-    case Bearing::NORTH:
+    case NORTH:
       robot_position.second = robot_position.second + 1;
       break;
-    case Bearing::EAST:
+    case EAST:
       robot_position.first = robot_position.first + 1;
       break;
-    case Bearing::SOUTH:
+    case SOUTH:
       robot_position.second = robot_position.second - 1;
       break;
-    case Bearing::WEST:
+    case WEST:
       robot_position.first = robot_position.first - 1;
       break;
   }
 }
 
-void Robot::execute_sequence(string sequence) {
-  for (char& action : sequence) {
+void Robot::execute_sequence(const string& sequence) {
+  for (const char& action : sequence) {
     switch (action) {
       case 'R':
         turn_right();
@@ -43,9 +47,5 @@ void Robot::execute_sequence(string sequence) {
         break;
     }
   }
-  if (sequence == "R") {
-    turn_right();
-  }
 }
-
 }  // namespace robot_simulator
--- robot-simulator/robot_simulator.h
+++ robot-simulator/robot_simulator.h
@@ -1,31 +1,25 @@
 #pragma once
 #include <string>
-#include <utility>
 
 namespace robot_simulator {
-using std::pair;
 using std::string;
+using Coordination = std::pair<int, int>;
+
 enum Bearing { NORTH, EAST, SOUTH, WEST };
 class Robot {
  private:
-  pair<int, int> robot_position;
-  Bearing robot_bearing;
+  Coordination robot_position{0, 0};
+  Bearing robot_bearing{NORTH};
 
  public:
-  Robot() {
-    robot_position = pair<int, int>(0, 0);
-    robot_bearing = Bearing::NORTH;
-  }
-  Robot(pair<int, int> robot_position, Bearing robot_bearing) {
-    this->robot_position = robot_position;
-    this->robot_bearing = robot_bearing;
-  }
-  pair<int, int> get_position() const { return robot_position; }
+  Robot();
+  Robot(Coordination robot_position, Bearing robot_bearing);
+  Coordination get_position() const { return robot_position; }
   Bearing get_bearing() const { return robot_bearing; }
   void turn_right();
   void turn_left();
   void advance();
-  void execute_sequence(string);
+  void execute_sequence(const string&);
 };
 
 }  // namespace robot_simulator
