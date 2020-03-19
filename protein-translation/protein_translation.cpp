03d5a6dfd220f11fd2669e970cd9e7510924c1e2
TQA <trinhquocanh94@gmail.com>
2020-03-19 22:48:17 +0100

    Test code coverage

--- protein-translation/protein_translation.cpp
+++ protein-translation/protein_translation.cpp
@@ -4,7 +4,13 @@
 namespace protein_translation {
 using std::map;
 
-map<string, string> translator = {{"AUG", "Methionine"}};
+map<string, string> translator = {
+    {"AUG", "Methionine"}, {"UUU", "Phenylalanine"}, {"UUC", "Phenylalanine"},
+    {"UUA", "Leucine"},    {"UUG", "Leucine"},       {"UCU", "Serine"},
+    {"UCC", "Serine"},     {"UCA", "Serine"},        {"UCG", "Serine"},
+    {"UAU", "Tyrosine"},   {"UAC", "Tyrosine"},      {"UGU", "Cysteine"},
+    {"UGC", "Cysteine"},   {"UGG", "Tryptophan"},    {"UAA", "STOP"},
+    {"UAG", "STOP"},       {"UGA", "STOP"}};
 vector<string> proteins(string adn) { return vector<string>{translator[adn]}; }
 
 }  // namespace protein_translation
