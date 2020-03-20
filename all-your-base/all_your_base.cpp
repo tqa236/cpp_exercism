4f58e67e2da74689ca07443a9fba8b3978ad1f94
TQA <trinhquocanh94@gmail.com>
2020-03-20 12:34:17 +0100

    Install codecov on TravisCI

--- all-your-base/all_your_base.cpp
+++ all-your-base/all_your_base.cpp
@@ -2,13 +2,18 @@
 
 #include <algorithm>
 #include <iostream>
+#include <stdexcept>
 #include <vector>
 
-using namespace std;
+using std::invalid_argument;
+using std::vector;
 
 namespace all_your_base {
 vector<unsigned int> convert(int in_base, vector<unsigned int> in_digits,
                              int out_base) {
+  if ((in_base <= 1) || (out_base <= 1)) {
+    throw invalid_argument("Invalid base.");
+  }
   int dec_num = to_dec(in_base, in_digits);
   vector<unsigned int> out_digits = to_base(dec_num, out_base);
   return out_digits;
