bd35e328e63758f5d25ceb82a15fdc5a034b87e0
tqa <trinhquocanh94@gmail.com>
2020-11-24 12:03:31 +0100

    Solve some exercises

--- triangle/triangle.cpp
+++ triangle/triangle.cpp
@@ -3,16 +3,14 @@
 #include <algorithm>
 #include <stdexcept>
 
-using namespace std;
-
 namespace triangle {
 static void check_valid_triangle(double const side1, double const side2,
                                  double const side3) {
-  if (min({side1, side2, side3}) <= 0) {
-    throw domain_error("Non-positive side is illegal.");
+  if (std::min({side1, side2, side3}) <= 0) {
+    throw std::domain_error("Non-positive side is illegal.");
   }
-  if (max({side1, side2, side3}) >= (side1 + side2 + side3) / 2) {
-    throw domain_error("Violate triangle inequality.");
+  if (std::max({side1, side2, side3}) >= (side1 + side2 + side3) / 2) {
+    throw std::domain_error("Violate triangle inequality.");
   }
 }
 
--- triangle/triangle.h
+++ triangle/triangle.h
@@ -1,10 +1,7 @@
-#if !defined(TRIANGLE_H)
-#define TRIANGLE_H
+#pragma once
 
 namespace triangle {
 enum flavor { equilateral, isosceles, scalene };
 flavor kind(double const, double const, double const);
 
 }  // namespace triangle
-
-#endif
