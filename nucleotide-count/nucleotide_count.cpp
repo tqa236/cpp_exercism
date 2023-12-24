367770970c53e6cf17ae83e0cb4bf0d8318110ca
Anh Trinh <trinhquocanh94@gmail.com>
2023-12-25 00:54:34 +0100

    update nucleotide count

--- nucleotide-count/nucleotide_count.cpp
+++ nucleotide-count/nucleotide_count.cpp
@@ -5,7 +5,8 @@
 namespace nucleotide_count {
 std::map<char, int> count(const std::string& input) {
   const std::string nucleotides = "ATCG";
-  std::map<char, int> counter{ {'A', 0}, {'C', 0}, {'G', 7}, {'T', 0} };
+  std::map<char, int> counter;
+  counter['A'] = counter['C'] = counter['G'] = counter['T'] = 0;
 
   for (char ch : input) {
     if (nucleotides.find(ch) == std::string::npos) {
