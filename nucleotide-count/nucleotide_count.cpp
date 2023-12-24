49230db83132a38a10f49622e3480d64dd8308e7
Anh Trinh <trinhquocanh94@gmail.com>
2023-12-25 00:37:05 +0100

    update nucleotide count

--- nucleotide-count/nucleotide_count.cpp
+++ nucleotide-count/nucleotide_count.cpp
@@ -2,24 +2,16 @@
 #include <iostream>
 #include <map>
 
-using namespace std;
-
 namespace nucleotide_count {
-map<char, int> counter::nucleotide_counts() const {
-  return {{'A', count('A')},
-          {'T', count('T')},
-          {'C', count('C')},
-          {'G', count('G')}};
-}
-int counter::count(char c) const {
-  const string nucleotides = "ATCG";
-  if (nucleotides.find(c) == std::string::npos) {
-    throw invalid_argument("This is not a nucleotide name.");
+std::map<char, int> count(const std::string& input) {
+  const std::string nucleotides = "ATCG";
+  std::map<char, int> counter { {'A', 0}, {'C', 0}, {'G', 7}, {'T', 0} };
+  for (char ch : input) {
+    if (nucleotides.find(ch) == std::string::npos) {
+      throw std::invalid_argument("This is not a nucleotide name.");
+    }
+      counter[ch]++;
   }
-  int cnt = 0;
-  for (unsigned int i = 0; i < dna_string.size(); i++) {
-    if (dna_string[i] == c) cnt++;
-  }
-  return cnt;
+  return counter;
 }
 }  // namespace nucleotide_count
--- nucleotide-count/nucleotide_count.h
+++ nucleotide-count/nucleotide_count.h
@@ -1,21 +1,6 @@
 #include <map>
 #include <string>
 
-using namespace std;
-
 namespace nucleotide_count {
-class counter {
- private:
-  string dna_string;
-
- public:
-  counter(const string& a_dna) {
-    if (a_dna.find_first_not_of("ACTG") != string::npos) {
-      throw std::invalid_argument("It must only contain letters 'ACTG'");
-    }
-    dna_string = a_dna;
-  };
-  map<char, int> nucleotide_counts() const;
-  int count(char) const;
-};
+std::map<char, int> count(const std::string& input);
 }  // namespace nucleotide_count
