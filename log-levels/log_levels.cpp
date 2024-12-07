df34c986b8f3c06b84c5a84f74a332b4c0bac315
Trinh Quoc Anh <trinhquocanh94@gmail.com>
2024-12-07 06:33:26 +0100

    Solve log levels

--- log-levels/log_levels.cpp
+++ log-levels/log_levels.cpp
@@ -1,27 +1,34 @@
 #include <string>
 
-namespace log_line {
-    std::string message(std::string line) {
+namespace log_line
+{
+    std::string message(std::string line)
+    {
         // return the message
         std::string delimiter = " ";
         size_t pos = line.find(delimiter);
         return line.substr(pos + delimiter.length());
     }
 
-    std::string log_level(std::string line) {
+    std::string log_level(std::string line)
+    {
         // return the log level
         std::string delimiter = " ";
         size_t pos = line.find(delimiter);
         std::string before = line.substr(0, pos);
-        if (before.length() > 1) {
+        if (before.length() > 1)
+        {
             before = before.substr(1, before.length() - 3);
-        } else {
+        }
+        else
+        {
             before = "";
         }
         return before;
-        }
+    }
 
-    std::string reformat(std::string line) {
+    std::string reformat(std::string line)
+    {
         // return the reformatted message
         return message(line) + " (" + log_level(line) + ")";
     }
